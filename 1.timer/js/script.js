f2645ec12f5286f75efeaa9e6a0766056a05347d
thanushayan <77339597+thanushayan@users.noreply.github.com>
2022-11-28 19:23:45 +0530

    Add files via upload

---
+++ 1.timer/js/script.js
@@ -0,0 +1,39 @@
+ ("#")
+ const days=document.querySelector("#days")
+ const hours=document.querySelector("#hours")
+ const Minutes=document.querySelector("#Minutes")
+ const Seconds=document.querySelector("#Seconds")
+
+  
+
+ 
+ function UpdateTime(){
+ const currentYear =new Date().getFullYear();
+
+ const newYear=new Date(`january 1 ${currentYear+1} 00:00:00`);
+
+ 
+
+ const currentDate=new Date();
+ 
+
+ const diff=newYear-currentDate;
+  const d =Math.floor(diff/1000/60/60/24);
+
+ 
+
+  const h=Math.floor((diff/1000/60/60)%24);
+
+ 
+  const m=Math.floor((diff/1000/60)%60);
+ 
+  const s=Math.floor((diff/1000)%60);
+ // console.log(h+" "+m+" "+s);
+
+ days.innerHTML= d<10?"0"+d:d;
+ hours.innerHTML=h<10?"0"+h:h;
+ Minutes.innerHTML= m<10?"0"+m:m;
+ Seconds.innerHTML=s<10?"0"+s:s;
+ }
+
+ setInterval(UpdateTime,1000);
